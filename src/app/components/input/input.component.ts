e775c772a63aa4c4ff974c38d0dd1a406c4779c2
Evan Derby <somekidwithhtml@gmail.com>
2017-12-28 23:48:54 -0700

    Add input box autoexpand, resolve #10.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -11,12 +11,31 @@ export class InputComponent implements OnInit {
   @ViewChild('box') private messageInput: ElementRef;
   @Output() messageSent = new EventEmitter<Message>();
 
+  baseScrollHeight: number = 0;
+
   attachBoxVisible: boolean = false;
   currentMessage: Message;
 
   ngOnInit() {
     this.resetMessage();
   }
+
+  textareaFocus(): void {
+    let savedValue = this.messageInput.nativeElement.value;
+    this.messageInput.nativeElement.value = "";
+    this.baseScrollHeight = this.messageInput.nativeElement.scrollHeight;
+    this.messageInput.nativeElement.value = savedValue;
+  }
+
+  textareaInput(): void {
+    let minRows = this.messageInput.nativeElement.getAttribute('data-min-rows') | 0, rows;
+    this.messageInput.nativeElement.rows = minRows;
+    let calc = (this.messageInput.nativeElement.scrollHeight - this.baseScrollHeight) / 16;
+    rows = Math.ceil(calc);
+    this.messageInput.nativeElement.rows = minRows + rows;
+  }
+
+  // autoexpanding textarea code comes from https://codepen.io/vsync/pen/frudD
 
   sendMessage(text: string) {
     this.currentMessage.text = text;
